ce3bfe79e0a2b2906688ecfbdc893b6cee915d73
Nikolay Shevchenko <shevnv@gmail.com>
2014-01-09 01:13:58 +0400

    add backup

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 VIM_DIR=~/dotfiles
 
+# Backup old .vim
+for i in $VIM_DIR ~/.bashrc ~/.gitconfig ~/.tmux.conf ; do [ -e $i ] && mv $i $i.old; done
+
 # Clone my vimrc
 git clone git://github.com/shevnv/dotfiles.git $VIM_DIR
 cd $VIM_DIR
