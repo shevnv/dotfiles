da14f7dfc24fcf087947f089dba2028e8caa6c25
Nikolay Shevchenko <shevnv@gmail.com>
2014-01-10 00:30:55 +0400

    change install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-VIM_DIR=~/dotfiles
+DIR=~/dotfiles
 
 # Backup old .vim
-for i in $VIM_DIR ~/.bashrc ~/.gitconfig ~/.tmux.conf ; do [ -e $i ] && mv $i $i.old; done
+for i in ~/.bashrc ~/.gitconfig ~/.tmux.conf $DIR; do [ -e $i ] && echo 'mv $i' && mv $i $i.old; done
 
 # Clone my vimrc
-git clone git://github.com/shevnv/dotfiles.git $VIM_DIR
-cd $VIM_DIR
+git clone git@github.com:shevnv/dotfiles.git $DIR
+cd $DIR
 
 # Update Pathogen bundle
 git submodule init; git submodule update
 
 # Add links to vimrc and gvimrc
-for i in bashrc gitconfig tmux.conf; do ln -s $VIM_DIR/$i ~/.$i; done
+for i in bashrc gitconfig tmux.conf; do ln -s $DIR/$i ~/.$i; done
 
 # Cleanup
 git clean -fd
